e51e5cc44e22f154bd17fc4e354e4cf3d8e92bc2
Infinite-8892 <zorro Zohrab@DESKTOP-VB1UK3R>
2023-02-24 21:33:20 +0400

    Medkit Fully Working

--- src/main/java/net/mcreator/blustoolpacks/procedures/MedKitRightClickProcedure.java
+++ src/main/java/net/mcreator/blustoolpacks/procedures/MedKitRightClickProcedure.java
@@ -1,7 +1,14 @@
 package net.mcreator.blustoolpacks.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+
+import net.minecraft.world.IWorld;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.item.ItemStack;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
@@ -10,8 +17,31 @@ import net.mcreator.blustoolpacks.item.MedKitItem;
 import net.mcreator.blustoolpacks.BlustoolpacksMod;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class MedKitRightClickProcedure {
+	@Mod.EventBusSubscriber
+	private static class GlobalTrigger {
+		@SubscribeEvent
+		public static void onRightClickItem(PlayerInteractEvent.RightClickItem event) {
+			PlayerEntity entity = event.getPlayer();
+			if (event.getHand() != entity.getActiveHand()) {
+				return;
+			}
+			double i = event.getPos().getX();
+			double j = event.getPos().getY();
+			double k = event.getPos().getZ();
+			IWorld world = event.getWorld();
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("x", i);
+			dependencies.put("y", j);
+			dependencies.put("z", k);
+			dependencies.put("world", world);
+			dependencies.put("entity", entity);
+			dependencies.put("event", event);
+			executeProcedure(dependencies);
+		}
+	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("entity") == null) {
@@ -20,15 +50,58 @@ public class MedKitRightClickProcedure {
 			return;
 		}
 		Entity entity = (Entity) dependencies.get("entity");
-		if (entity instanceof LivingEntity)
-			((LivingEntity) entity).setHealth((float) 100);
-		if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
-			((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("You have healed yourself"), (true));
-		}
-		if (entity instanceof PlayerEntity) {
-			ItemStack _stktoremove = new ItemStack(MedKitItem.body);
-			((PlayerEntity) entity).inventory.func_234564_a_(p -> _stktoremove.getItem() == p.getItem(), (int) 1,
-					((PlayerEntity) entity).container.func_234641_j_());
+		if (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem() == MedKitItem.body) {
+			if (entity.isSneaking() == true) {
+				if (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.CHEST) : ItemStack.EMPTY)
+						.getItem() == MedKitItem.body) {
+					if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+						((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("You already have a MedKit equiped on your back"), (true));
+					}
+				} else if (((entity instanceof LivingEntity)
+						? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.CHEST)
+						: ItemStack.EMPTY).getItem() == (ItemStack.EMPTY).getItem()) {
+					if (entity instanceof LivingEntity) {
+						if (entity instanceof PlayerEntity)
+							((PlayerEntity) entity).inventory.armorInventory.set((int) 2, new ItemStack(MedKitItem.body));
+						else
+							((LivingEntity) entity).setItemStackToSlot(EquipmentSlotType.CHEST, new ItemStack(MedKitItem.body));
+						if (entity instanceof ServerPlayerEntity)
+							((ServerPlayerEntity) entity).inventory.markDirty();
+					}
+					if (entity instanceof PlayerEntity) {
+						ItemStack _stktoremove = new ItemStack(MedKitItem.body);
+						((PlayerEntity) entity).inventory.func_234564_a_(p -> _stktoremove.getItem() == p.getItem(), (int) 1,
+								((PlayerEntity) entity).container.func_234641_j_());
+					}
+				} else {
+					if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+						((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("You have something else equipped..."), (true));
+					}
+				}
+			} else {
+				if (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHealth() : -1) >= 20 && ((entity instanceof LivingEntity)
+						? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.CHEST)
+						: ItemStack.EMPTY).getItem() == (ItemStack.EMPTY).getItem()) {
+					if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+						((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("Your health is already at max, equiped the bag."), (true));
+					}
+				} else if (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHealth() : -1) >= 20) {
+					if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+						((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("Your health is already at max"), (true));
+					}
+				} else {
+					if (entity instanceof LivingEntity)
+						((LivingEntity) entity).setHealth((float) 100);
+					if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+						((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("You have healed yourself"), (true));
+					}
+					if (entity instanceof PlayerEntity) {
+						ItemStack _stktoremove = new ItemStack(MedKitItem.body);
+						((PlayerEntity) entity).inventory.func_234564_a_(p -> _stktoremove.getItem() == p.getItem(), (int) 1,
+								((PlayerEntity) entity).container.func_234641_j_());
+					}
+				}
+			}
 		}
 	}
 }
