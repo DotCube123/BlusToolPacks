53ea26f824f671a8f47b9ded77e2e787e6dfcec4
Infinite-8892 <zorro Zohrab@DESKTOP-VB1UK3R>
2023-02-24 23:58:46 +0400

    HealthBar

---
+++ src/main/java/net/mcreator/blustoolpacks/procedures/HealthBarProcedureProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.blustoolpacks.procedures;
+
+import java.util.Map;
+
+public class HealthBarProcedureProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
